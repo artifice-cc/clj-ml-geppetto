0d95e9baf774d50ab49e117ef3d9adfef28d46b1
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-09-21 20:08:33 -0400

    Added default clojure project structure

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-ml-geppetto
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clj_ml_geppetto/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-ml-geppetto.core-test
+  (:require [clojure.test :refer :all]
+            [clj-ml-geppetto.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
