d53acedbd288ea7264eabf6036566cc0d0471156
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-09-22 12:34:21 -0400

    Basic ability to run an experiment

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject clj-ml-geppetto "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clj-ml utilizing the geppetto framework"
+  :url "http://geppetto.artifice.cc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cc.artifice/clj-ml "0.4.0-SNAPSHOT"]
+                 [cc.artifice/geppetto "2.5.0-SNAPSHOT"]]
+  :main clj-ml-geppetto.core)
--- src/clj_ml_geppetto/core.clj
+++ src/clj_ml_geppetto/core.clj
@@ -1,6 +1,31 @@
-(ns clj-ml-geppetto.core)
+(ns clj-ml-geppetto.core
+  (:use [geppetto.cli])
+  (:use [clj-ml io classifiers data utils]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def classifier-table
+  {"c45" [:decision-tree :c45]
+   "boosted-stump" [:decision-tree :boosted-stump]
+   "random-forest" [:decision-tree :random-forest]
+   "rotation-forest" [:decision-tree :rotation-forest]
+   "m5p" [:decision-tree :m5p]
+   "naive-bayes" [:bayes :naive]
+   "smo" [:support-vector-machine :smo]
+   "spegasos" [:support-vector-machine :spegasos]
+   "svm" [:support-vector-machine :libsvm-grid]
+   "linear-reg" [:regression :linear]
+   "logistic-reg" [:regression :logistic]
+   "pace-reg" [:regression :pace]
+   "boosted-reg" [:regression :boosted-regression]
+   "mlp" [:neural-network :multilayer-perceptron]})
+
+(defn run
+  [comparative? params]
+  (let [ds (-> (load-instances :arff "http://clj-ml.artifice.cc/iris.arff")
+               (dataset-set-class :class))
+        classifier (apply make-classifier (classifier-table (:Classifier params)))
+        evaluation (classifier-evaluate classifier :cross-validation ds 10)]
+    [(assoc (select-keys evaluation [:error-rate])
+       :params params)]))
+
+(defn -main [& args]
+  (geppetto-cli run args))
